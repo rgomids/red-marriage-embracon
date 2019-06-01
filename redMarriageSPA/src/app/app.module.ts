d153e330cb89591310347a10168843108c855a5c
Gustavo <gvnv.2000@gmail.com>
2019-06-01 16:14:16 -0300

    Components setup

--- redMarriageSPA/src/app/app.module.ts
+++ redMarriageSPA/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    TimelineComponent,
+    FornecedoresComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
