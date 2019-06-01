342f8261e2bc823c15788315db09f3c8310c0dd5
Rafael Gomides <mrrafagomides@gmail.com>
2019-06-01 19:38:24 -0300

    routers e swagger

--- redMarriageAPI/routers/router.go
+++ redMarriageAPI/routers/router.go
@@ -1,5 +1,38 @@
 package routers
 
-func init() {
+import (
+	"redMarriageAPI/controllers"
 
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/api",
+		beego.NSNamespace("/account",
+			beego.NSInclude(
+				&controllers.AccountController{},
+			),
+		),
+		beego.NSNamespace("/step",
+			beego.NSInclude(
+				&controllers.StepController{},
+			),
+		),
+		beego.NSNamespace("/timelines",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+		beego.NSNamespace("/wedding",
+			beego.NSInclude(
+				&controllers.WeddingController{},
+			),
+		),
+		beego.NSNamespace("/weddingprocess",
+			beego.NSInclude(
+				&controllers.WeddingProcessController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
 }
